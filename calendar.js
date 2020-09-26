4e1b20685a8fd843930420a2d5fff09afe817d46
a-ssk <fabledfabled@gmail.com>
2020-09-26 10:29:37 +0900

    update 日曜日を後半に移動する

--- calendar.js
+++ calendar.js
@@ -9,64 +9,66 @@ jQuery(function ($) {
     // 今月を設定する
     function setMonth() {
         let date = new Date();
-        $("#dateYearMonth").text(date.getFullYear() + "." + (date.getMonth() + 1));
+        $("#yearMonth").text(date.getFullYear() + "." + (date.getMonth() + 1));
     }
 
     // 年月に対応したカレンダーを設定する
     function setDay() {
         $("#areaWeek").nextAll().remove();
 
-        let dateData = $("#dateYearMonth").text().split(".");
+        let yearMonth = $("#yearMonth").text().split(".");
         // 今月を取得する
-        let firstDay = new Date(dateData[0], parseInt(dateData[1]) - 1, 1);
-        let firstDayOfMonth = firstDay.getDay();
+        let firstDay = new Date(yearMonth[0], parseInt(yearMonth[1]) - 1, 1);
+        let firstDayOfWeek = firstDay.getDay();
         // 今月の末日を取得する
-        let lastDay = new Date(dateData[0], dateData[1], 0);
-        let lastDayOfMonth = lastDay.getDay();
+        let lastDay = new Date(yearMonth[0], yearMonth[1], 0);
+        let lastDayOfWeek = lastDay.getDay();
 
         let day = 1;
-        let str = "";
+        let weekStr = "";
         while (true) {
-            str += "<tr>";
-            for (i = 0; i < 7; i++) {
-                // 初週かつ初日より前の場合は、空白にする
-                if (day == 1 && i < firstDayOfMonth) {
-                    str += "<td></td>";
-                    // 最終週かつ最終日より後の場合は、空白にする
-                } else if (day >= lastDay.getDate() && lastDayOfMonth < i) {
-                    str += "<td></td>";
+            weekStr += "<tr>";
+            const list = [1, 2, 3, 4, 5, 6, 0];
+            for (const num of list) {
+                if (day == 1 && firstDayOfWeek > num) {
+                    // 初週かつ初日より前の場合、空白にする
+                    weekStr += "<td></td>";
+                } else if (day > lastDay.getDate()) {
+                    // 最終週かつ最終日より後の場合、空白にする
+                    weekStr += "<td></td>";
                 } else {
-                    str += "<td>";
-                    if (i == 0 || i == 6) {
-                        str += "<a class='holiday'>" + day++ + "</a>";
+                    if (num == 0 || num == 6) {
+                        // 土日の場合
+                        weekStr += "<td class='holiday'>" + day++ + "</td>";
                     } else {
-                        str += day++;
+                        // 平日の場合
+                        weekStr += "<td>" + day++ + "</td>";
                     }
-                    str += "</td>";
                 }
             }
-            str += "</tr>";
+            weekStr += "</tr>";
 
-            if (lastDay.getDate() + 1 == day) {
+            // 最終日の場合、ループ終了
+            if (lastDay.getDate() + 1 <= day) {
                 break;
             }
         }
-        $("#calendar").append(str);
+        $("#calendar").append(weekStr);
     }
 
     // 先月を設定する
     $(document).on("click", "#btnPreviousMonth", function () {
-        let dateData = $("#dateYearMonth").text().split(".");
-        let preMonth = new Date(dateData[0], parseInt(dateData[1]) - 2, 1);
-        $("#dateYearMonth").text(preMonth.getFullYear() + "." + (preMonth.getMonth() + 1));
+        let yearMonth = $("#yearMonth").text().split(".");
+        let preMonth = new Date(yearMonth[0], parseInt(yearMonth[1]) - 2, 1);
+        $("#yearMonth").text(preMonth.getFullYear() + "." + (preMonth.getMonth() + 1));
         setDay();
     })
 
     // 翌月を設定する
     $(document).on("click", "#btnNextMonth", function () {
-        let dateData = $("#dateYearMonth").text().split(".");
-        let nextMonth = new Date(dateData[0], parseInt(dateData[1]), 1);
-        $("#dateYearMonth").text(nextMonth.getFullYear() + "." + (nextMonth.getMonth() + 1));
+        let yearMonth = $("#yearMonth").text().split(".");
+        let nextMonth = new Date(yearMonth[0], parseInt(yearMonth[1]), 1);
+        $("#yearMonth").text(nextMonth.getFullYear() + "." + (nextMonth.getMonth() + 1));
         setDay();
     })
 
